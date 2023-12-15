e50151823777080a9e1a9855bf8f874776d46c45
ren3459 <ren3459@gmail.com>
2023-12-15 21:58:18 +0300

    начало TODO App

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -7,6 +7,7 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Это твоя вторая страница смотри сюда</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
